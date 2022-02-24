e9b3b15ccdc347c505c2740e1fe87892886bed77
Oscar fariña <61348449+ofaricamp@users.noreply.github.com>
2022-02-24 12:27:19 +0100

    Add files via upload

---
+++ SevBol3_Ejer3/SevBol3_Ejer3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SevBol3_Ejer3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int puerto = 10823;
+            bool apagar = false;
+            IPEndPoint ie = new IPEndPoint(IPAddress.Any, puerto);
+
+            using (Socket s = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp))
+            {
+
+            }
+        }
+    }
+}
